a89d063dbae2e67ec01da76099351866bb27b3d6
dadachan <vincent8397@gmail.com>
2018-12-10 14:27:50 +0800

    Add check police nearby amount

---
+++ analysis/check_police.py
@@ -0,0 +1,26 @@
+import csv
+
+locs = csv.DictReader(open('../data/locCategory.csv', newline=''))
+polices = []
+
+for loc in locs:
+    if loc['Type'] == 'Police Station':
+        loc['Amount'] = 0
+        polices.append(loc)
+
+def nearby(police, loc):
+    if (abs(float(police['Latitude']) - float(loc['Latitude'])) <= 0.001 or abs(float(police['Longitude']) - float(loc['Longitude'])) <= 0.001):
+        police['Amount'] += 1
+
+crimes = csv.DictReader(open('../data/Crimes2016.csv', newline=''))
+
+fieldnames = ['Latitude', 'Longitude', 'Type', 'Amount']
+writer = csv.DictWriter(open('./police_amount.csv', 'w'), fieldnames=fieldnames)
+writer.writeheader()
+
+for crime in crimes:
+    for police in polices:
+        nearby(police, crime)
+
+for police in polices:
+    writer.writerow(police)
