16a4fb04873967e16e6f845b7207897e01d3703b
dadachan <vincent8397@gmail.com>
2018-12-12 14:00:33 +0800

    Modify make positive

--- analysis/check_month.py
+++ analysis/check_month.py
@@ -55,8 +55,46 @@ for month in months:
         'WEAPONS VIOLATION': 0
     }
 
+total = {
+    'ARSON': 0,
+    'ASSAULT': 0,
+    'BATTERY': 0,
+    'BURGLARY': 0,
+    'CONCEALED CARRY LICENSE VIOLATION': 0,
+    'CRIM SEXUAL ASSAULT': 0,
+    'CRIMINAL DAMAGE': 0,
+    'CRIMINAL TRESPASS': 0,
+    'DECEPTIVE PRACTICE': 0,
+    'GAMBLING': 0,
+    'HOMICIDE': 0,
+    'HUMAN TRAFFICKING': 0,
+    'INTERFERENCE WITH PUBLIC OFFICER': 0,
+    'INTIMIDATION': 0,
+    'KIDNAPPING': 0,
+    'LIQUOR LAW VIOLATION': 0,
+    'MOTOR VEHICLE THEFT': 0,
+    'NARCOTICS': 0,
+    'NON - CRIMINAL': 0,
+    'NON-CRIMINAL': 0,
+    'NON-CRIMINAL (SUBJECT SPECIFIED)': 0,
+    'OBSCENITY': 0,
+    'OFFENSE INVOLVING CHILDREN': 0,
+    'OTHER NARCOTIC VIOLATION': 0,
+    'OTHER OFFENSE': 0,
+    'PROSTITUTION': 0,
+    'PUBLIC INDECENCY': 0,
+    'PUBLIC PEACE VIOLATION': 0,
+    'ROBBERY': 0,
+    'SEX OFFENSE': 0,
+    'STALKING': 0,
+    'THEFT': 0,
+    'WEAPONS VIOLATION': 0
+}
+
 for loc in locs:
     months[loc['Date'][:2]][loc['Primary Type']] += 1
+    total[loc['Primary Type']] += 1
 
 pp = pprint.PrettyPrinter(indent=4)
 pp.pprint(months)
+pp.pprint(total)
--- tools/make_positive.py
+++ tools/make_positive.py
@@ -4,9 +4,23 @@ with open('../data/Crimes2016.csv', newline='') as crimeFile:
     rows = csv.DictReader(crimeFile)
 
     fieldnames = ['Date', 'Primary Type', 'Latitude', 'Longitude', 'Time slot']
+    ignore = [
+        'CONCEALED CARRY LICENSE VIOLATION',
+        'GAMBLING',
+        'HUMAN TRAFFICKING',
+        'NON - CRIMINAL',
+        'NON-CRIMINAL',
+        'NON-CRIMINAL (SUBJECT SPECIFIED)',
+        'OBSCENITY',
+        'OTHER NARCOTIC VIOLATION',
+        'PUBLIC INDECENCY'
+    ]
     writer = csv.DictWriter(open('../data/positive.csv', 'w'), fieldnames=fieldnames)
     writer.writeheader()
+    count = 0
     for row in rows:
+        if row['Primary Type'] in ignore:
+            continue
         hour = int(row['Date'][-11:-9])
         if 'PM' in row['Date']:
             if hour != 12:
@@ -19,4 +33,6 @@ with open('../data/Crimes2016.csv', newline='') as crimeFile:
         time_slot = ['midnight', 'morning', 'afternoon', 'night']
         row['Time slot'] = time_slot[int(hour/6)]
         writer.writerow(row)
+        count += 1
+    print('Positive data: ' + str(count))
     
