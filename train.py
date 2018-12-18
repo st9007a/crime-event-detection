7ab43206b456c8f908d28ab098a02a2603d8ea80
uno <st9007a@gmail.com>
2018-12-18 09:59:19 +0800

    Model aggeration

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from pprint import pprint
+import pickle
 
 import numpy as np
 import pandas as pd
@@ -20,13 +21,12 @@ numerical_features = [
     'wind_direction',
     'wind_speed',
     'population_density',
-    'lat', 'lon',
+    'night_spot_density',
     'category',
-    'second_type',
-    # 'root_type',
     'time_slot',
     'weather_description',
-    # 'month',
+    'lat',
+    'lon',
     'hour',
 ]
 
@@ -46,15 +46,11 @@ def build_features(df):
 
     return numerical
 
-def get_features():
+def get_features(pos, neg1, neg2, seed=0):
 
-    pos = pd.read_csv('./data/positive_samples.csv', delimiter=',')
-    neg1 = pd.read_csv('./data/negative1_samples.csv', delimiter=',')
-    neg2 = pd.read_csv('./data/negative2_samples.csv', delimiter=',')
+    new_neg1 = neg1.sample(frac=0.3, random_state=seed)
 
-    # neg1 = neg1.sample(frac=0.6, random_state=0)
-
-    res = pd.concat([pos, neg1, neg2])
+    res = pd.concat([pos, new_neg1, neg2])
     train_set = res[res['set'] == 'train']
     valid_set = res[res['set'] == 'valid']
     test_set = res[res['set'] == 'test']
@@ -63,31 +59,37 @@ def get_features():
 
 if __name__ == '__main__':
 
-    x_train, x_valid, x_test, y_train, y_valid, y_test = get_features()
+    pos = pd.read_csv('./data/positive_samples.csv', delimiter=',')
+    neg1 = pd.read_csv('./data/negative1_samples.csv', delimiter=',')
+    neg2 = pd.read_csv('./data/negative2_samples.csv', delimiter=',')
 
-    y_train = np.where(y_train > 0, 1, 0)
-    y_valid = np.where(y_valid > 0, 1, 0)
-    y_test = np.where(y_test > 0, 1, 0)
+    for i in range(9):
+        x_train, x_valid, x_test, y_train, y_valid, y_test = get_features(pos, neg1, neg2, seed=i)
 
-    dtrain = xgb.DMatrix(x_train, label=y_train, feature_names=numerical_features)
-    dvalid = xgb.DMatrix(x_valid, label=y_valid, feature_names=numerical_features)
-    dtest = xgb.DMatrix(x_test, label=y_test, feature_names=numerical_features)
+        y_train = np.where(y_train > 0, 1, 0)
+        y_valid = np.where(y_valid > 0, 1, 0)
+        y_test = np.where(y_test > 0, 1, 0)
 
-    params = {
-        'max_depth': 5,
-        'eta': 0.1,
-        'objective': 'binary:logistic',
-        'eval_metric': 'error',
-        'silent': 1,
-    }
+        dtrain = xgb.DMatrix(x_train, label=y_train, feature_names=numerical_features)
+        dvalid = xgb.DMatrix(x_valid, label=y_valid, feature_names=numerical_features)
+        dtest = xgb.DMatrix(x_test, label=y_test, feature_names=numerical_features)
 
-    evallist = [(dvalid, 'eval')]
+        params = {
+            'max_depth': 5,
+            'eta': 1,
+            'objective': 'binary:logistic',
+            'eval_metric': 'error',
+            'silent': 1,
+            'seed': 1,
+        }
 
-    bst = xgb.train(params, dtrain, num_boost_round=1000, evals=evallist, early_stopping_rounds=10)
+        bst = xgb.train(params, dtrain, num_boost_round=200, evals=[(dvalid, 'eval')], early_stopping_rounds=10)
 
-    pred = bst.predict(dtest, ntree_limit=bst.best_ntree_limit)
-    pred = np.where(pred > 0.5, 1, 0)
-    pprint(bst.get_score())
-    print(classification_report(y_test, pred))
+        pred = bst.predict(dtest, ntree_limit=bst.best_ntree_limit)
+        print(pred)
+        pred = np.where(pred > 0.5, 1, 0)
+        pprint(bst.get_score())
+        print(classification_report(y_test, pred))
 
-    bst.save_model('model.xgb')
+        with open('models/model.%d.pkl' % i, 'wb') as p:
+            pickle.dump(bst, p, protocol=pickle.HIGHEST_PROTOCOL)
